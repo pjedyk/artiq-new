e0dfc93c1e182f0319b9c49a0ad9f67800004717
Piotr Jedyk <jedykp@gmail.com>
2025-03-14 18:05:15 +0100

    no axi in lib.rs

--- boards/genesys_zu-5ev/2_firmware/src/lib.rs
+++ boards/genesys_zu-5ev/2_firmware/src/lib.rs
@@ -1,17 +1,14 @@
 #![no_std]
 
 use core::panic::PanicInfo;
-use core::ptr::read_volatile;
 
 #[no_mangle]
 extern "C" fn rust_main() -> ! {
     let mut _x: u32 = 0;
-    let addr: u32 = 0x80000000;
 
     loop {
-        unsafe {
-            _x = read_volatile(addr as *const u32);
-        }
+        _x += 1;
+        _x -=1;
     }
 }
 
