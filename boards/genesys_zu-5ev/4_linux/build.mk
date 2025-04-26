376d18f1afa127f82c92cd7881b3e4b1eab24ec3
Piotr Jedyk <jedykp@gmail.com>
2025-04-26 11:03:18 +0200

    4_linux

--- boards/genesys_zu-5ev/4_linux/build.mk
+++ boards/genesys_zu-5ev/4_linux/build.mk
@@ -1,5 +1,6 @@
 LINUX := $(THIS)
 
+MACHINE := genesys_zu-5ev
 BITBAKE_IMAGE := artiqlinux-image-default
 
 I_LINUX := $(I)/$(LINUX)
@@ -18,12 +19,13 @@ O_ZYNQMP_ARTIQ_CONF := $(O_LINUX_CONF_MACHINE)/zynqmp-artiq.conf
 O_all_CONF := $(O_BBLAYERS_CONF) $(O_LOCAL_CONF) $(O_ZYNQMP_ARTIQ_CONF)
 O_SETUPSDK := $(O_LINUX)/setupsdk
 O_SDT_TOP_BIT := $(O_VITIS_WS)/hw_pf/hw/sdt/top.bit
-O_IMAGES := $(O_LINUX)/tmp/deploy/images/xlnx-zynqmp
-O_IMAGE_WIC := $(O_IMAGES)/$(BITBAKE_IMAGE)-xlnx-zynqmp.wic
+O_GEN_MACHINECONF_CONF := $(O_LINUX_CONF)/gen-machineconf.conf
+O_GEN_MACHINECONF_FLAG := $(O_LINUX)/.gen-machineconf.flag
+O_BITBAKE_FLAG := $(O_LINUX)/.bitbake.flag
 
 .PHONY: $(LINUX)
 all: $(LINUX)
-$(LINUX): $(O_IMAGE_WIC)
+$(LINUX): $(O_BITBAKE_FLAG)
 
 $(O_LINUX_CONF): | $(O_LINUX)
 $(O_LINUX_CONF_MACHINE): | $(O_LINUX_CONF)
@@ -35,19 +37,25 @@ $(O_LOCAL_CONF): $(I_LOCAL_CONF_IN) | $(O_LINUX_CONF)
 $(O_ZYNQMP_ARTIQ_CONF): $(I_ZYNQMP_ARTIQ_CONF_IN) | $(O_LINUX_CONF_MACHINE)
 $(O_all_CONF):
 	m4 -D_YOCTO_="$(I_YOCTO)" -D_LINUX_="$(I_LINUX)" -D_BUILD_="$(O)" \
-	  -D_FSBL_FILE_="$(basename $(O_FSBL_ELF))" -- "$(^)" >"$(@)"
+	  -D_FSBL_FILE_="$(basename $(O_FSBL_ELF))" \
+	  -D_GEN_MACHINECONF_CONF_="$(O_GEN_MACHINECONF_CONF)" \
+	  -- "$(^)" >"$(@)"
 
 $(O_SETUPSDK):
 	printf -- 'source -- %q %q\n' "$(I_SETUPSDK)" "$(O_LINUX)" >"$(O_SETUPSDK)"
 
 $(O_SDT_TOP_BIT): $(O_TOP_BIT) $(O_FSBL_ELF)
-	cp -fT -- $(O_TOP_BIT) $(O_SDT_TOP_BIT)
+	cp -fT -- "$(O_TOP_BIT)" "$(O_SDT_TOP_BIT)"
 
-$(O_IMAGE_WIC): $(O_all_CONF) $(O_SETUPSDK) $(O_SDT_TOP_BIT)
-	source -- "$(O_SETUPSDK)" && \
-	gen-machineconf \
+$(O_GEN_MACHINECONF_FLAG): $(O_all_CONF) $(O_SETUPSDK) $(O_SDT_TOP_BIT)
+	source -- "$(O_SETUPSDK)" && gen-machineconf \
 	  --add-config='CONFIG_YOCTO_BBMC_CORTEXA53_FSBL=n' \
 	  --add-config='CONFIG_YOCTO_INCLUDE_MACHINE_NAME="zynqmp-artiq"' \
 	  --hw-description="$(dir $(O_SDT_TOP_BIT))" \
-	  parse-sdt -l "$(O_LOCAL_CONF)" && \
-	bitbake -- $(BITBAKE_IMAGE)
+	  --machine-name="$(MACHINE)" \
+	  parse-sdt -l "$(O_GEN_MACHINECONF_CONF)"
+	touch -- "$(O_GEN_MACHINECONF_FLAG)"
+
+$(O_BITBAKE_FLAG): $(O_GEN_MACHINECONF_FLAG)
+	source -- "$(O_SETUPSDK)" && bitbake -- "$(BITBAKE_IMAGE)"
+	touch -- "$(O_BITBAKE_FLAG)"
